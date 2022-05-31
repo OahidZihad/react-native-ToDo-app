3016b135b8f62b484ecb6e15e097124e6456784c
OahidZihad <oahidzihad1@gmail.com>
2022-05-31 13:34:25 +0600

    Modal

--- components/AlertToast.js
+++ components/AlertToast.js
@@ -6,15 +6,17 @@ import {
   Pressable,
   Alert,
   ToastAndroid,
+  Modal,
 } from 'react-native';
 import React, {useState} from 'react';
 
 const AlertToast = () => {
   const [name, setName] = useState('');
   const [submitted, setSubmitted] = useState(false);
+  const [showWarning, setShowWarning] = useState(false);
 
   const onPressHandler = () => {
-    if (name.length > 3) {
+    if (name.length >= 3) {
       setSubmitted(!submitted);
     } else {
       //   Alert.alert(
@@ -36,25 +38,53 @@ const AlertToast = () => {
       //     ],
       //     {cancelable: true, onDismiss: () => console.warn('Alert Dismissed')},
       //   );
-
       //   ToastAndroid.show('You are okay', ToastAndroid.SHORT);
       //   ToastAndroid.showWithGravity(
       //     'You are okay',
       //     ToastAndroid.SHORT,
       //     ToastAndroid.CENTER,
       //   );
-      ToastAndroid.showWithGravityAndOffset(
-        'You are okay',
-        ToastAndroid.SHORT,
-        ToastAndroid.TOP,
-        0,
-        200,
-      );
+      //   ToastAndroid.showWithGravityAndOffset(
+      //     'You are okay',
+      //     ToastAndroid.SHORT,
+      //     ToastAndroid.TOP,
+      //     0,
+      //     200,
+      //   );
+
+      setShowWarning(true);
     }
   };
 
   return (
     <View style={styles.body}>
+      <Modal
+        animationType="slide"
+        transparent={true}
+        hardwareAccelerated
+        visible={showWarning}
+        onRequestClose={() => setShowWarning(false)}>
+        <View style={styles.centered_view}>
+          <View style={styles.warning_modal}>
+            <View style={styles.warning_title}>
+              <Text style={[styles.text, {fontWeight: '500', fontSize: 26}]}>
+                WARNING!
+              </Text>
+            </View>
+            <Text style={[styles.text, {textAlign: 'center', fontSize: 22}]}>
+              Name must be 3 char
+            </Text>
+            <Pressable
+              style={styles.warning_button}
+              android_ripple={{color: 'blue'}}
+              onPress={() => setShowWarning(false)}>
+              <Text style={[styles.text, {fontWeight: '500', fontSize: 26}]}>
+                OK
+              </Text>
+            </Pressable>
+          </View>
+        </View>
+      </Modal>
       <Text style={styles.text}>Please Write Your Name: </Text>
       <TextInput
         style={styles.input}
@@ -86,7 +116,6 @@ export default AlertToast;
 const styles = StyleSheet.create({
   body: {
     flex: 1,
-    backgroundColor: 'white',
     alignItems: 'center',
   },
   input: {
@@ -111,5 +140,43 @@ const styles = StyleSheet.create({
     color: 'white',
     alignItems: 'center',
     borderRadius: 15,
+  },
+  centered_view: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#00000070',
+  },
+  warning_modal: {
+    width: 300,
+    height: 300,
+    borderRadius: 20,
+    backgroundColor: 'white',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderColor: 'gray',
+    borderWidth: 1,
+  },
+  warning_title: {
+    width: '100%',
+    backgroundColor: 'yellow',
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'absolute',
+    top: 0,
+    borderTopRightRadius: 20,
+    borderTopLeftRadius: 20,
+  },
+  warning_button: {
+    width: '100%',
+    backgroundColor: 'cyan',
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'absolute',
+    bottom: 0,
+    borderBottomLeftRadius: 20,
+    borderBottomRightRadius: 20,
   },
 });
